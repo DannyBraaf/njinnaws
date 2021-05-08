f572c7d9393a68b5512ae05d5764688b93c9da4c
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-09 01:37:31 +0200

    asdfasdf

---
+++ src/eclistallregions.py
@@ -0,0 +1,15 @@
+import boto3
+from botocore.config import Config
+
+class EcCreateInstance:
+
+    def run(self):
+        
+
+        ec2 = boto3.resource('ec2',region_name=self.region)
+        
+      # List all regions
+        client = boto3.client('ec2')
+        regions = [region['RegionName'] for region in client.describe_regions()['Regions']]
+        
+        return regions
