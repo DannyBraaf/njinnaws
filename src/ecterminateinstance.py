02c1b8e4380167c57dcb11cf89e4b2e706524c66
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-08 22:44:09 +0200

    sdfasdf

---
+++ src/ecterminateinstance.py
@@ -0,0 +1,11 @@
+import boto3
+
+
+class EcTerminateInstance:
+
+    def run(self):
+
+ec2 = boto3.resource('ec2')
+
+list1 = [self.Instanceids]
+ec2.instances.filter(InstanceIds = list1).terminate()
