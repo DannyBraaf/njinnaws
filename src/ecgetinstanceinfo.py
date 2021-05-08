210b7595017e0e82e226225db8b6229a4d660428
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-09 00:37:52 +0200

    sdfsdf

--- src/ecgetinstanceinfo.py
+++ src/ecgetinstanceinfo.py
@@ -1,19 +1,19 @@
 import boto3
 from botocore.config import Config
 
+
 class EcGetinstanceinfo:
 
     def run(self):
 
-
-        client = boto3.client('ec2',region_name=self.region)
-        Myec2=client.describe_instances( 
-                      InstanceIds=[
-                      self.Instanceids,
-                      ],
-                      DryRun=self.dryrunEnabled,
-                      MaxResults=123,
-                      NextToken='string')
-                      print(Myec2)     
+        client = boto3.client('ec2', region_name=self.region)
+        Myec2 = client.describe_instances(
+            InstanceIds=[
+                self.Instanceids,
+            ],
+            DryRun=self.dryrunEnabled,
+            MaxResults=123,
+            NextToken='string')
+        print(Myec2)
 
         return Myec2
