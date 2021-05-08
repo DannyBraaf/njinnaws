da22735e79974d56bd7cf33dabe8fbb3280fbdf8
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-08 23:03:40 +0200

    sdf

--- src/ecterminateinstance.py
+++ src/ecterminateinstance.py
@@ -5,10 +5,9 @@ class EcTerminateInstance:
 
     def run(self):
 
-ec2 = boto3.resource('ec2')
+       ec2 = boto3.resource('ec2')
+       list1 = [self.Instanceids]
+       ec2.instances.filter(InstanceIds = list1).terminate()
 
-list1 = [self.Instanceids]
-ec2.instances.filter(InstanceIds = list1).terminate()
 
-
-    return list1
+       return list1
