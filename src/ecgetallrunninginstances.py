d8a0304fb35da5e1fe944c0c0c5b2d66bdb864b6
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-09 16:08:21 +0200

    asdfsdf

--- src/ecgetallrunninginstances.py
+++ src/ecgetallrunninginstances.py
@@ -3,25 +3,30 @@ import boto3
 
 class Execute:
 
-    def run(self):
+  def run(self):
 
-        ec2 = boto3.resource('ec2', region_name='eu-west-1')
-        dict1 = {}
-        instances = ec2.instances.filter(
-            Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+      ec2 = boto3.resource('ec2', region_name='eu-west-1')
+      dict1 = {}
+      instances = ec2.instances.filter(
+          Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
 
-        for instance in instances:
-            print(instance.id, instance.instance_type, instance.vpc.id, instance.subnet.id,
-                  instance.state['Name'], instance.public_ip_address, instance.private_ip_address, instance.architecture, instance.launch_time)
-            dict2 = {"instance_id": instance.id,
-                     "instance_type": instance.instance_type,
-                     "instance_vpc_id": instance.vpc.id,
-                     "instance_subnet_id": instance.subnet.id,
-                     "instance_state": instance.state['Name'],
-                     "instance_public_ip_address": instance.public_ip_address,
-                     "instance_private_ip_address": instance.private_ip_address,
-                     "instance_architecture": instance.architecture,
-                     "instance_launch_time": instance.launch_time}
-            dict1.update(dict2)
+      for instance in instances:
+          print(instance.id, instance.instance_type, instance.vpc.id, instance.subnet.id,
+                instance.state['Name'], instance.public_ip_address, instance.private_ip_address, instance.architecture, instance.launch_time)
+          dict2 = ["instance_id": instance.id,
+                  "instance_type": instance.instance_type,
+                  "instance_vpc_id": instance.vpc.id,
+                  "instance_subnet_id": instance.subnet.id,
+                  "instance_state": instance.state['Name'],
+                  "instance_public_ip_address": instance.public_ip_address,
+                  "instance_private_ip_address": instance.private_ip_address,
+                  "instance_architecture": instance.architecture,
+                  "instance_launch_time": instance.launch_time]
+          # print(dict1)
+          dict1.append(dict2)
+         
+      return dict1
 
-        return dict1
+
+
+x.update(d)
