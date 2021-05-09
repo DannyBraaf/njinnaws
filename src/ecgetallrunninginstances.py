1449e83b7d6009b84bb7076950c693c69863303d
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-09 16:11:22 +0200

    sdfsdf

--- src/ecgetallrunninginstances.py
+++ src/ecgetallrunninginstances.py
@@ -13,15 +13,8 @@ class Execute:
       for instance in instances:
           print(instance.id, instance.instance_type, instance.vpc.id, instance.subnet.id,
                 instance.state['Name'], instance.public_ip_address, instance.private_ip_address, instance.architecture, instance.launch_time)
-          dict2 = ["instance_id": instance.id,
-                  "instance_type": instance.instance_type,
-                  "instance_vpc_id": instance.vpc.id,
-                  "instance_subnet_id": instance.subnet.id,
-                  "instance_state": instance.state['Name'],
-                  "instance_public_ip_address": instance.public_ip_address,
-                  "instance_private_ip_address": instance.private_ip_address,
-                  "instance_architecture": instance.architecture,
-                  "instance_launch_time": instance.launch_time]
+          dict2 = [instance.id, instance.instance_type, instance.vpc.id, instance.subnet.id,
+                instance.state['Name'], instance.public_ip_address, instance.private_ip_address, instance.architecture, instance.launch_time]
           # print(dict1)
           dict1.append(dict2)
          
