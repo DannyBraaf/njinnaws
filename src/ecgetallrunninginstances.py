6288d5efd3b262d82bbc59e4bfcaed2151455167
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-09 02:35:53 +0200

    asdfsdf

--- src/ecgetallrunninginstances.py
+++ src/ecgetallrunninginstances.py
@@ -7,7 +7,7 @@ class Execute:
     def run(self):
 
         ec2 = boto3.resource('ec2')
-          for instance in ec2.instances.all():
+        for instance in ec2.instances.all():
           print(instance.id, instance.state)
 
           return ec2.instances.all()
