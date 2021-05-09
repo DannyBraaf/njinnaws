374d30809f5f63cfadc0112769e2c38d2f8dbf08
DannyBraaf <49520410+DannyBraaf@users.noreply.github.com>
2021-05-10 01:38:56 +0200

    sdfsdf

--- src/eccreateloadbalancer.py
+++ src/eccreateloadbalancer.py
@@ -8,6 +8,16 @@ class Execute:
         client = boto3.client('elb', region_name=self.region)
         response = client.create_load_balancer(
             LoadBalancerName=self.name,
+            if not self.SSLCertificateId:
+            Listeners=[
+                {
+                    'Protocol': self.Protokol,
+                    'LoadBalancerPort': self.LoadBalancerPort,
+                    'InstanceProtocol': self.InstanceProtocol,
+                    'InstancePort': self.InstancePort                  
+                },
+            ],
+            else
             Listeners=[
                 {
                     'Protocol': self.Protokol,
